d016c03f3e9f6d9178be33f5d28d6a462695c917
Ilya <92110133+xiosamagh@users.noreply.github.com>
2022-06-27 18:31:46 +0300

    add rest url

--- root/{{entity.name}}/routes/{{entity.nameUpper}}ApiRoutes.java
+++ root/{{entity.name}}/routes/{{entity.nameUpper}}ApiRoutes.java
@@ -3,6 +3,6 @@ package {{path}}.{{entity.name}}.routes;
 import {{path}}.base.routers.BaseApiRoutes;
 
 public class {{entity.nameUpper}}ApiRoutes {
-    public static final String ROOT = BaseApiRoutes.V1 + "/{{entity.name}}";
+    public static final String ROOT = BaseApiRoutes.V1 + "/{{entity.restApiUrl}}";
     public static final String BY_ID = ROOT + "/{id}";
 }
